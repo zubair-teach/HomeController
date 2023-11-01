e683ca0af8a8c794c17c16a1ed571e9c49a1d195
Zubair Ahmed <zubair@szabist-isb.edu.pk>
2023-11-01 14:35:18 +0500

    HomeControlLauncher.java Skeleton Code

--- HomeControlLauncher.java
+++ HomeControlLauncher.java
@@ -1,1 +1,36 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Iterator;
+  public class HomeControlLauncher{
+ArrayList<HomeDevice> Things = new ArrayList <HomeDevice>();
+  
+	public void goToSleep () {
+				
+	}
+			
+
+		
+    }
+	public void wakeup () {
+				
+	}
+	
+	public void add (HomeDevice thing) {
+	
+	}
+  
+  
+  public static void main (String [] args) {
+	  HomeControlLauncher hc = new HomeControlLauncher(); 
+	  
+	  hc.add (new RoomLight("bedroom"));
+	  hc.add (new EmergencyLight("hallway"));
+	  hc.add (new CDPlayer ("bedroom"));
+	  hc.add (new TV ("sitting room"));
+	  
+	  hc.goToSleep ();
+	  hc.wakeup ();
+   }
+   }
+
 
